45855b4e71538830ca118ad5efa6e188e55db1f2
Purwandi <free6300@gmail.com>
2018-01-11 11:58:12 +0700

    Try to fixing group controller test

--- tests/Controllers/GroupControllerTest.php
+++ tests/Controllers/GroupControllerTest.php
@@ -9,15 +9,15 @@ class GroupControllerTest extends AbstractControllerTest
 {
     public function test_create_group()
     {
+        $group = \App\Group::make([
+            'name' => 'Project name',
+            'slug' => 'project-name',
+            'description' => 'Awesome project name',
+        ]);
+        $group->id = 1;
+
         $groups = Mockery::mock(GroupRepository::class);
-        $groups->shouldReceive('create')->andReturn(
-            (object)[
-                'id' => 1,
-                'name' => 'Project name',
-                'slug' => 'project-name',
-                'description' => 'Awesome project name',
-            ]
-        );
+        $groups->shouldReceive('create')->andReturn($group);
 
         $this->app->instance(GroupRepository::class, $groups);
         $response = $this->actingAs(factory(\App\User::class)->make(), 'api')
@@ -40,7 +40,7 @@ class GroupControllerTest extends AbstractControllerTest
     public function test_invalid_post_data()
     {
         $groups = Mockery::mock(GroupRepository::class);
-        $groups->shouldReceive('create')->andReturn([]);
+        $groups->shouldReceive('create')->andReturn(null);
 
         $this->app->instance(GroupRepository::class, $groups);
         
